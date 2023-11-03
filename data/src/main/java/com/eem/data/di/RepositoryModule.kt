a73f6c24de6180fb7859df3effde1b33b599a301
Elias Eguizabal <elias.eguizabal@goluuk.com>
2023-11-03 02:30:36 -0600

    feat(App): Add Repositories to DI module

--- data/src/main/java/com/eem/data/di/RepositoryModule.kt
+++ data/src/main/java/com/eem/data/di/RepositoryModule.kt
@@ -1,9 +1,16 @@
 package com.eem.data.di
 
 import com.eem.data.network.api.AuthApi
+import com.eem.data.network.api.SurveyApi
+import com.eem.data.network.api.UserApi
 import com.eem.data.network.model.CoroutineContextProvider
 import com.eem.data.repository.AuthRepositoryImpl
+import com.eem.data.repository.SurveyRepositoryImpl
+import com.eem.data.repository.UserRepositoryImpl
+import com.eem.data.room.dao.AccessTokenDao
 import com.eem.domain.repository.auth.AuthRepository
+import com.eem.domain.repository.survey.SurveyRepository
+import com.eem.domain.repository.user.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,6 +30,21 @@ object RepositoryModule {
     @Provides
     fun provideAuthRepository(
         authApi: AuthApi,
+        accessTokenDao: AccessTokenDao,
         contextProvider: CoroutineContextProvider
-    ): AuthRepository = AuthRepositoryImpl(authApi, contextProvider)
+    ): AuthRepository = AuthRepositoryImpl(authApi, accessTokenDao, contextProvider)
+
+    @Provides
+    fun provideSurveyRepository(
+        surveyApi: SurveyApi,
+        accessTokenDao: AccessTokenDao,
+        contextProvider: CoroutineContextProvider
+    ): SurveyRepository = SurveyRepositoryImpl(surveyApi, accessTokenDao, contextProvider)
+
+    @Provides
+    fun provideUserRepository(
+        userApi: UserApi,
+        accessTokenDao: AccessTokenDao,
+        contextProvider: CoroutineContextProvider
+    ): UserRepository = UserRepositoryImpl(userApi, accessTokenDao, contextProvider)
 }
