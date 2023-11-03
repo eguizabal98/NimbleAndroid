86835ce3981b9b0ab450269bc27b5d7a39aa9b68
Elias Eguizabal <elias.eguizabal@goluuk.com>
2023-11-03 09:55:12 -0600

    feat(App): Create Splash Screen

---
+++ app/src/main/java/com/eem/nimble/presentation/ui/splash/SplashScreen.kt
@@ -0,0 +1,50 @@
+package com.eem.nimble.presentation.ui.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eem.nimble.R
+import com.eem.nimble.presentation.theme.NimbleAndroidTheme
+
+@Composable
+fun SplashScreen() {
+    Box {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.splash_background),
+            contentDescription = "Background Nimble",
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.width(160.dp),
+                painter = painterResource(id = R.drawable.logo_white),
+                contentDescription = "Logo Nimble",
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NimbleAndroidTheme {
+        SplashScreen()
+    }
+}
